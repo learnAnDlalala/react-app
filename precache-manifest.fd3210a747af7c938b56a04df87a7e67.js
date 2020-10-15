ed15f1de30cd7a026d0020c1c50426460e57c180
learnANDlalala <thetelevators@gmail.com>
2020-10-15 06:00:25 +0400

    Updates

---
+++ precache-manifest.fd3210a747af7c938b56a04df87a7e67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f5fafa65e4727198ef01a92f995ec7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c4a534069bed6ac5e077",
+    "url": "/react-app/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "a8cb0ead319f67218323",
+    "url": "/react-app/static/css/main.ded66616.chunk.css"
+  },
+  {
+    "revision": "c4a534069bed6ac5e077",
+    "url": "/react-app/static/js/2.2bf5e1e8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-app/static/js/2.2bf5e1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cb0ead319f67218323",
+    "url": "/react-app/static/js/main.e797ec6d.chunk.js"
+  },
+  {
+    "revision": "1917207b184409547094",
+    "url": "/react-app/static/js/runtime-main.29e9e9d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/react-app/precache-manifest.5648c090b32eded852628c8c0ee52809.js"
+  "/react-app/precache-manifest.fd3210a747af7c938b56a04df87a7e67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
